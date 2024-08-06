4a17ea916d6bcfca8b6e1e1629735e1906b599e3
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-06 22:55:05 +0200

    Created the class clapTrap

---
+++ ex00/ClapTrap.hpp
@@ -0,0 +1,23 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+class ClapTrap
+{
+private:
+	std::string _name;
+	int hitPoints;
+	int energyPoints;
+	int attackDamage;
+public:
+	ClapTrap(void);
+	ClapTrap(const ClapTrap &toCopy);
+	ClapTrap &operator=(const ClapTrap &toCopy);
+	void attack(const std::string &target);
+	void takeDamage(unsigned int amount);
+	void beRepaired(unsigned int amount);
+	~ClapTrap();
+};
+#endif
