5e0445247f3dab46d6e41aff103cc0a7634a97b2
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-07 16:01:28 +0200

    ScavTrap methods have been all implemented

--- ex01/ScavTrap.cpp
+++ ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 12:55:52 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/07 13:50:04 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/07 16:00:55 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,4 +58,36 @@ void ScavTrap::attack(const std::string &target)
 	std::cout << "ScavTrap " << this->_name << " attacks " << target << ", causing "
 			  << this->_attackDamage << " points of damage!" << std::endl;
 	this->_energyPoints--;
+}
+
+void ScavTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hitPoints <= 0)
+		return;
+	int lifeLeft = this->_hitPoints - (int)amount;
+	this->_hitPoints = lifeLeft < 0 ? 0 : lifeLeft;
+	std::cout << "Scavtrap " << this->_name << " takes " << amount << " points of damage"
+			  << " which leaves it with " << this->_hitPoints << " hit points!"
+			  << std::endl;
+	if (this->_hitPoints == 0)
+		std::cout << "Scavtrap " << this->_name << " has been brutally killed!!!" << std::endl;
+}
+
+void ScavTrap::beRepaired(unsigned int amount)
+{
+	if (this->_energyPoints <= 0 || this->_hitPoints <= 0)
+	{
+		std::cout << "Scavtrap " << this->_name << " can't repair itself, it is done for"
+				  << std::endl;
+		return;
+	}
+	this->_hitPoints += (int)amount;
+	std::cout << "Scavtrap " << this->_name << " repairs " << amount << " hit points "
+			  << "which leaves it with " << this->_hitPoints << " hit points" << std::endl;
+	this->_energyPoints--;
+}
+
+void ScavTrap::guardGate(void)
+{
+	std::cout << "Scavtrap " << this->_name << " is in Gate keeper mode" << std::endl;
 }
