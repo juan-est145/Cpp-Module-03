b12ec09d3f029b312c70280c6b7c8113646cfb87
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-07 16:46:49 +0200

    Implemented default constructor for FragTrap

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,8 +6,16 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 16:27:16 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/07 16:27:22 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/07 16:40:29 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "FragTrap.hpp"
+
+FragTrap::FragTrap(void) : ClapTrap()
+{
+	std::cout << "Fragtrap default constructor has been called" << std::endl;
+	this->_hitPoints = 100;
+	this->_energyPoints = 100;
+	this->_attackDamage = 30;
+}
