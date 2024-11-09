047250be2004df283cb24126cc868e6bf3cd7c3e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-11-09 16:46:23 +0100

    Updated destructors of clapTrap to virtual ones

--- ex01/ClapTrap.hpp
+++ ex01/ClapTrap.hpp
@@ -20,6 +20,6 @@ public:
 	void attack(const std::string &target);
 	void takeDamage(unsigned int amount);
 	void beRepaired(unsigned int amount);
-	~ClapTrap(void);
+	virtual ~ClapTrap(void);
 };
 #endif
